84012385f63a103ba2ca8c3e7029875a1d585501
hunter-mcgriff <111534236+hunter-mcgriff@users.noreply.github.com>
2022-10-13 15:05:23 -0500

    Update app.js

--- app.js
+++ app.js
@@ -17,36 +17,77 @@ app.set('view engine', 'ejs');
 
 let pName, pNumber, pPosition, players = '';
 
-app.get('/', async function (req, res) {
+ app.get('/', (req, res) => {
+        db.collection('quebec-numbers').find().toArray()
+          .then(results => {
+            res.render('index.ejs', { players: results})
+          })
+          .catch(error => console.error(error))
+        // res.render('index.ejs', {})  
+      })
 
-    client.connect;
+      app.post('/players', (req, res) => {
+        playersCollection.insertOne(req.body)
+          .then(result => {
+            res.redirect('/')
+          })
+          .catch(error => console.error(error))
+      })
 
-    databasesList = await client.db().admin().listDatabases();
- 
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+      // app.post('/players', (req,res) => {
+      //     console.log(req.body);
+      // })
 
-
-    const collection = client.db("myFirstDatabse").collection("posts");
+    //   app.delete('/players/:id', (req,res)=>{
+    //     const id=req.params.id;
     
-    console.log('connected!');
-       
-    const result = await collection.findOne(); //.toArray();
-    
-    console.log(result);
-            
-    //res.send(result);
+    //     playersCollection.findOneAndDelete(id)
+    //       .then(data=>{
+    //         if(!data){
+    //             res.status(404).send({message: "cannot work"})
+    //         }else{
+    //             res.send({
+    //                 message:"User was deleted"
+    //             })
+    //         }
+    //       })
+    //       .catch(err=>{
+    //         res.status(500).send({
+    //             message:"cannot delete"
+    //         });
+    //       });
+    // })
 
-    res.render('index' ,{
-      pName: result.name, 
-      pPosition: result.position,
-      pNumber: result.number, 
-      players: players
+    app.post('/deletePlayer/:id', async (req,res)=>{
+      
+      console.log("id", req.params.id); 
+      // console.log("name", req.sparams.name); 
 
+      let result = await playersCollection.findOneAndDelete( 
+        {
+        // _id : `"ObjectId("${req.params.id}")"`
+        // "_id": ObjectId("6340b4a0716efae98339f1d7")
+        // "_id": "ObjectId(\"6340b4a0716efae98339f1d7\")"
+        // "_id" : "ObjectId(6340b4a0716efae98339f1d7)"
+        // _id : req.params.id
+        // name :  req.params.name
+        // "_id" :  `ObjectId('${req.params.id}')`
+        // _id :  ObjectId('${req.params.id}')
+        // "_id" : "6340ba72e3120ac27bd0ea9c"
+        // "_id.$oid" : "6340ba72e3120ac27bd0ea9c"
+        "_id": ObjectId(req.params.id)
 
+        // name : "adfs"
+
+       }
+      )
+      .then(result => {
+        //res.json(`Deleted Darth Vader's quote`)
+        console.log(result); 
+        res.redirect('/');
+      })
+      .catch(error => console.error(error))
     })
-          
-});
 
     
 
