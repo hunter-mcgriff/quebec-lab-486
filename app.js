cca7e4f94958e1399fbb1ff6c86307bc13c410ad
hunter-mcgriff <111534236+hunter-mcgriff@users.noreply.github.com>
2022-10-17 13:43:57 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const express = require('express')
 const path = require('node:path')
 const bodyParser = require('body-parser')
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
 const client = new MongoClient(process.env.uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
 
 console.log(process.env.uri);
 
@@ -15,90 +13,36 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs');
 
-let pName, pNumber, pPosition, players = '';
-
 
 app.get('/', async function (req, res) {
+    client.connect;
+    databasesList = await client.db().admin().listDatabases();
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
 
-  client.connect;
-
-  databasesList = await client.db().admin().listDatabases();
-
-  console.log("Databases:");
-  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-
-
-  const collection = client.db("myFirstDatabse").collection("posts");
-
-  console.log('connected!');
-
-  const result = await collection.findOne(); //.toArray();
-
-  console.log(result);
-
-  //res.send(result);
-
-  res.render('index' ,{
-    pName: result.name,
-    pPosition: result.position,
-    pNumber: result.number,
-    players: players
-
-
-  })
-
-
-
-
-     
-app.post('/players', async (req, res) => {
-
-  try {
-    // console.log("req.body: ", req.body) 
-    client.connect; 
-    const collection = client.db("myFirstDatabase").collection("posts");
-    await collection.insertOne(req.body);
-      
-    res.redirect('/');
-  }
-  catch(e){
-    console.log(error)
-  }
-  finally{
-   // client.close()
-  }
-
-})
-  
-app.post('/deletePlayers/:id', async (req, res) => {
-
-  try {
-    console.log("req.params.id: ", req.params.id) 
+    const collection = client.db("myFirstDatabse").collection("posts");
+    console.log('connected!');
+       
+    const result = await collection.find().toArray();
     
-    client.connect; 
-    const collection = client.db("myFirstDatabase").collection("posts");
-    let result = await collection.findOneAndDelete( 
-      {
-        "_id": ObjectId(req.params.id)
-      }
-    )
+    console.log(result);
+            
+    //res.send(result);
+
+    res.render('index' ,{ players : result    })
+    
+ app.post('/players', (req, res) => {
+ console.log(req.body); 
+    client.connect;
+    const collection = client.db("myFirstDatabse").collection("posts");
+    collection.insertOne(req.body)
     .then(result => {
-      console.log(result); 
-      res.redirect('/');
-    })
+      res.redirect('/')
+     })
     .catch(error => console.error(error))
-  }
-  finally{
-    //client.close()
-  }
-
 })
 
-//app.listen(PORT, () => {
-  //console.log(`Server is running & listening on port ${PORT}`);
-//});
 
-    
           
 });
     
