cf7b016f450b53cfab72fdc4149361c3ed73341a
hunter-mcgriff <111534236+hunter-mcgriff@users.noreply.github.com>
2022-10-17 19:28:20 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const express = require('express')
-const app = express()
 const path = require('node:path')
 const bodyParser = require('body-parser')
 const { urlencoded } = require('body-parser')
