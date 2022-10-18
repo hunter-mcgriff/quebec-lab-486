6f8b869109b3995cdfca97686425e0cdb4b6415d
hunter-mcgriff <111534236+hunter-mcgriff@users.noreply.github.com>
2022-10-17 19:26:35 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 const express = require('express')
+const app = express()
 const path = require('node:path')
 const bodyParser = require('body-parser')
+const { urlencoded } = require('body-parser')
 const { ObjectId } = require('mongodb')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const client = new MongoClient(process.env.uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
